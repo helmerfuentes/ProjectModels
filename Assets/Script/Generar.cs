edc0e9254a430d9b9a98bd262ca6799937da0aa3
Helmer Fuentes Alvarado <37910514+helmerfuentes@users.noreply.github.com>
2019-07-11 23:39:32 -0500

    rutas en la zona rural

---
+++ Assets/Script/Generar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generar : MonoBehaviour
+{
+    
+    public GameObject [] coches;
+    public Component componente;
+    private bool enablecoches=true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Throwcoches());
+
+   
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    IEnumerator Throwcoches()
+    {
+        yield return new WaitForSeconds(2.0f);
+        while (enablecoches)
+        {
+            
+            hoMove ruta = new hoMove();
+            
+
+            GameObject coch = (GameObject) Instantiate(coches[Random.Range(0,coches.Length)], new Vector3 (235, 0.03616625f, 11.46f), Quaternion.identity);
+            //coch.AddComponent<hoMove>();    
+            //coch.GetComponent<hoMove>().speed = Random.Range(5,10);
+            //coch.GetComponent<hoMove>().pathContainer = (PathManager)GameObject.Find("cariilDR");
+            yield return new WaitForSeconds(5.0f);
+
+            
+        }
+
+    }
+}
