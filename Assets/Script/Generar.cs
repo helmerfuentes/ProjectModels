05a118b7abee61ec06ec5f60d52bbff5a074973d
Helmer Fuentes Alvarado <37910514+helmerfuentes@users.noreply.github.com>
2019-07-24 20:50:26 -0500

    restrcturacion Scena

--- Assets/Script/Generar.cs
+++ Assets/Script/Generar.cs
@@ -35,9 +35,9 @@ public class Generar : MonoBehaviour
             
 
             GameObject coch = (GameObject) Instantiate(coches[Random.Range(0,coches.Length)], new Vector3 (235, 0.03616625f, 11.46f), Quaternion.identity);
-            //coch.AddComponent<hoMove>();    
-            //coch.GetComponent<hoMove>().speed = Random.Range(5,10);
-            //coch.GetComponent<hoMove>().pathContainer = (PathManager)GameObject.Find("cariilDR");
+            coch.AddComponent<hoMove>();
+            coch.GetComponent<hoMove>().onStart=false;
+            coch.GetComponent<hoMove>().pathContainer = (PathManager)GameObject.Find("cariilDR");
             yield return new WaitForSeconds(5.0f);
 
             
---
+++ Assets/Script/camara.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camara : MonoBehaviour
+{
+    public Camera camara1;
+    public Camera camara2;
+    public Camera camara3;
+    public Camera camara4;
+    public Camera camara5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        if (Input.GetButtonDown("Cam1"))
+        {
+            camara1.enabled = true;
+            camara2.enabled = false;
+            camara3.enabled = false;
+            camara4.enabled = false;
+            camara5.enabled = false;
+        }
+       
+
+        if (Input.GetButtonDown("Cam2"))
+        {
+            camara1.enabled = false;
+            camara2.enabled = true;
+            camara3.enabled = false;
+            camara4.enabled = false;
+            camara5.enabled = false;
+        }
+        if (Input.GetButtonDown("Cam3"))
+        {
+            camara1.enabled = false;
+            camara2.enabled = false;
+            camara3.enabled = true;
+            camara4.enabled = false;
+            camara5.enabled = false;
+        }
+        if (Input.GetButtonDown("Cam4"))
+        {
+            camara1.enabled = false;
+            camara2.enabled = false;
+            camara3.enabled = false;
+            camara4.enabled = true;
+            camara5.enabled = false;
+        }
+        if (Input.GetButtonDown("Cam5"))
+        {
+            camara1.enabled = false;
+            camara2.enabled = false;
+            camara3.enabled = false;
+            camara4.enabled = false;
+            camara5.enabled = true;
+        }
+
+    }
+}
